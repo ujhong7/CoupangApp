7ce851449d07f26a4a073d6fe0faf20832d344ee
유재홍 <ujhong7@gmail.com>
2024-09-20 00:06:11 +0900

    디자인 리소스 컴포넌트화

---
+++ CoupangApp/CoupangApp/DesignSource/CPColor.swift
@@ -0,0 +1,28 @@
+//
+//  CPColor.swift
+//  CoupangApp
+//
+//  Created by yujaehong on 9/19/24.
+//
+
+import UIKit
+
+enum CPColor {
+    static var bk: UIColor = UIColor(named: "bk")!
+    static var wh: UIColor = UIColor(named: "wh")!
+    static var coral: UIColor = UIColor(named: "coral")!
+    static var yellow: UIColor = UIColor(named: "yellow")!
+    static var keyColorRed: UIColor = UIColor(named: "key-color-red")!
+    static var keyColorRed2: UIColor = UIColor(named: "key-color-red-2")!
+    static var keyColorBlue: UIColor = UIColor(named: "key-color-blue")!
+    static var keyColorBlueTrans: UIColor = UIColor(named: "key-color-blue-trans")!
+    static var keyColorGreen: UIColor = UIColor(named: "key-color-green")!
+    static var keyColorGrey0: UIColor = UIColor(named: "key-color-grey-0")!
+    static var keyColorGrey1: UIColor = UIColor(named: "key-color-grey-1")!
+    static var keyColorGrey2: UIColor = UIColor(named: "key-color-grey-2")!
+    static var keyColorGrey3: UIColor = UIColor(named: "key-color-grey-3")!
+    static var keyColorGrey4: UIColor = UIColor(named: "key-color-grey-4")!
+    static var keyColorGrey5: UIColor = UIColor(named: "key-color-grey-5")!
+    static var keyColorGrey6: UIColor = UIColor(named: "key-color-grey-6")!
+    static var keyColorGrey7: UIColor = UIColor(named: "key-color-grey-7")!
+}
---
+++ CoupangApp/CoupangApp/DesignSource/CPFont.swift
@@ -0,0 +1,28 @@
+//
+//  CPFont.swift
+//  CoupangApp
+//
+//  Created by yujaehong on 9/20/24.
+//
+
+import UIKit
+
+enum CPFont {
+    static var regular10: UIFont = UIFont.systemFont(ofSize: 10, weight: .regular)
+    
+    static var regular12: UIFont = UIFont.systemFont(ofSize: 12, weight: .regular)
+    static var medium12: UIFont = UIFont.systemFont(ofSize: 12, weight: .medium)
+    static var bold12: UIFont = UIFont.systemFont(ofSize: 12, weight: .bold)
+    
+    static var regular14: UIFont = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static var bold14: UIFont = UIFont.systemFont(ofSize: 14, weight: .bold)
+    
+    static var medium16: UIFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static var bold16: UIFont = UIFont.systemFont(ofSize: 16, weight: .bold)
+    
+    static var light17: UIFont = UIFont.systemFont(ofSize: 17, weight: .light)
+    static var medium17: UIFont = UIFont.systemFont(ofSize: 17, weight: .medium)
+    static var bold17: UIFont = UIFont.systemFont(ofSize: 17, weight: .bold)
+    
+    static var bold20: UIFont = UIFont.systemFont(ofSize: 20, weight: .bold)
+}
