b2c1542cac6db8b7d1e1d93342c565bddb7a2f61
유재홍 <ujhong7@gmail.com>
2024-09-20 18:31:12 +0900

    HomeProductCollectionViewCell 생성

--- CoupangApp/CoupangApp/DesignSource/CPColor.swift
+++ CoupangApp/CoupangApp/DesignSource/CPColor.swift
@@ -12,19 +12,20 @@ enum CPColor {
     static var wh: UIColor = UIColor(named: "wh")!
     static var coral: UIColor = UIColor(named: "coral")!
     static var yellow: UIColor = UIColor(named: "yellow")!
+    
     static var keyColorRed: UIColor = UIColor(named: "key-color-red")!
     static var keyColorRed2: UIColor = UIColor(named: "key-color-red-2")!
     static var keyColorBlue: UIColor = UIColor(named: "key-color-blue")!
     static var keyColorBlueTrans: UIColor = UIColor(named: "key-color-blue-trans")!
-    
     static var keyColorGreen: UIColor = UIColor(named: "key-color-green")!
+    
     static var gray0: UIColor = UIColor(named: "gray-0")!
     static var gray1: UIColor = UIColor(named: "gray-1")!
     static var gray2: UIColor = UIColor(named: "gray-2")!
     static var gray3: UIColor = UIColor(named: "gray-3")!
-    static var gray3Cool: UIColor = UIColor(named: "gray-4")!
-    static var gray4: UIColor = UIColor(named: "gray-5")!
-    static var gray5: UIColor = UIColor(named: "gray-6")!
-    static var gray7: UIColor = UIColor(named: "gray-7")!
-//    static var gray3Cool: UIColor = UIColor(named: "gray-3-cool")!
+    static var gray3Cool: UIColor = UIColor(named: "gray-3-cool")!
+    static var gray4: UIColor = UIColor(named: "gray-4")!
+    static var gray5: UIColor = UIColor(named: "gray-5")!
+    
+    static var icon: UIColor = UIColor(named: "icon")!
 }
---
+++ CoupangApp/CoupangApp/Feature/Home/HomeProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeProductCollectionViewCell.swift
+//  CoupangApp
+//
+//  Created by yujaehong on 9/20/24.
+//
+
+import UIKit
+
+class HomeProductCollectionViewCell: UICollectionViewCell {
+    
+}
